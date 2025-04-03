809fbc77fdd450c15baf94272a31d0d7f4db7b3a
jeroenpg <jeroenpg@gmail.com>
2025-04-03 17:11:57 +0200

    remove

--- src/routes/view/nodots/endpoint.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute } from '@tanstack/solid-router';
-
-export const Route = createFileRoute('/view/nodots/endpoint')({
-  component: () => {
-    return <div>Hello, world from nodots!</div>;
-  }
-})
--- src/routes/view/something.with.dots/endpoint.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute } from '@tanstack/solid-router';
-
-export const Route = createFileRoute('/view/something.with.dots/endpoint')({
-  component: () => {
-    return <div>Hello, world from dots!</div>;
-  }
-})
