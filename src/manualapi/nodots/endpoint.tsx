42f3bd014f78880a7b6621178c1e7025d9ea8aff
jeroenpg <jeroenpg@gmail.com>
2025-04-03 17:11:25 +0200

    remove

--- src/manualapi/nodots/endpoint.tsx
+++
@@ -1,7 +0,0 @@
-import { createAPIFileRoute } from '@tanstack/solid-start/api';
-
-export const APIRoute = createAPIFileRoute('/api/nodots/endpoint')({
-  GET: (get) => {
-    return new Response('Hello, world from nodots!')
-  }
-})
--- src/manualapi/something.with.dots/endpoint.ts
+++
@@ -1,7 +0,0 @@
-import { createAPIFileRoute } from '@tanstack/solid-start/api';
-
-export const APIRoute = createAPIFileRoute('/api/so2332mething.with.dots/endpoint')({
-  GET: async (get) => {
-    return new Response('Hello, world from dots!')
-  }
-})
